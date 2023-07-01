314a92b1f295028fdaef856540da49227b0cd421
Christian <c.espinoza.code@gmail.com>
2023-07-01 14:27:41 -0500

    batman modal implemented

--- src/App.jsx
+++ src/App.jsx
@@ -69,6 +69,11 @@ function App() {
               <ModalRobin />
             </Modal>
           )}
+          {openModalBatman && (
+            <Modal>
+              <ModalBatman />
+            </Modal>
+          )}
         </section>
       </div>
     </>
