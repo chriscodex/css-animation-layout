e468016f384e74228022694e5e3b045684954cae
Christian <c.espinoza.code@gmail.com>
2023-06-30 23:51:59 -0500

    card slider structure added to index.jsx

--- src/components/Modals/Spiderman/index.jsx
+++ src/components/Modals/Spiderman/index.jsx
@@ -3,35 +3,38 @@ import { AppContext } from '../../../context';
 import './styles.css';
 
 function ModalSpiderman() {
-  const {setOpenModalSpiderman} = useContext(AppContext)
-  
+  const { setOpenModalSpiderman } = useContext(AppContext);
+
   const closeSpidermanModal = () => {
-    setOpenModalSpiderman(false)
-  }
-  
+    setOpenModalSpiderman(false);
+  };
+
   return (
     <div className="modal-spiderman">
       <div className="modal-content">
         <img
-          className="modal-spiderman-img"
+          className="modal-spiderman--close"
           src="/src/assets/close.svg"
           alt="close"
           onClick={() => closeSpidermanModal()}
         />
-        <div className='modal__content--slider'>
-          <input type="radio" name='slider-1' id='radio-1' checked/>
-          <input type="radio" name='slider-2' id='radio-2'/>
-          <input type="radio" name='slider-3' id='radio-3'/>
+        <div className="modal__content--slider">
+          <input type="radio" name="slider-1" id="radio-1" checked />
+          <input type="radio" name="slider-1" id="radio-2" checked />
+          <input type="radio" name="slider-1" id="radio-3" checked />
 
-          <div className='modal-spiderman__cards'>
-            <label htmlFor="radio-1" id='card-1' className='modal-spiderman__card'>
-              <img src="" alt="" />
+          <div className="cards">
+            <label htmlFor="radio-1" id="card-1" className="card">
+              <img
+              />
             </label>
-            <label htmlFor="radio-2" id='card-2' className='modal-spiderman__card'>
-              <img src="" alt="" />
+            <label htmlFor="radio-2" id="card-2" className="card">
+              <img
+              />
             </label>
-            <label htmlFor="radio-3" id='card-3' className='modal-spiderman__card'>
-              <img src="" alt="" />
+            <label htmlFor="radio-3" id="card-3" className="card">
+              <img
+              />
             </label>
           </div>
         </div>
