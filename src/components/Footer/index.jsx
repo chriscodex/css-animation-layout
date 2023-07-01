a5e8c6ff47dd79c0a6987e0d7c12e009bd319d7b
Christian <c.espinoza.code@gmail.com>
2023-07-01 14:39:02 -0500

    handlers added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,21 +3,27 @@ import { AppContext } from '../../context';
 import './styles.css';
 
 function Footer() {
-  const {setOpenModalSpiderman} = useContext(AppContext)
+  const {setOpenModalSpiderman, setOpenModalRobin, setOpenModalBatman} = useContext(AppContext)
   
-  const openModalHandler = () => {
+  const openModalSpidermanHandler = () => {
     setOpenModalSpiderman(true)
+  }
+  const openModalRobinHandler = () => {
+    setOpenModalRobin(true)
+  }
+  const openModalBatmanHandler = () => {
+    setOpenModalBatman(true)
   }
   
   return (
     <div className='footer'>
-      <div onClick={() => openModalHandler()}>
+      <div onClick={() => openModalSpidermanHandler()}>
         <img src="https://i.ibb.co/tKWqw8J/spiderman.png" alt="spiderman" />
       </div>
-      <div>
+      <div onClick={() => openModalRobinHandler()}>
         <img src="https://i.ibb.co/Xzsdvgg/robin.png" alt="robin" />
       </div>
-      <div>
+      <div onClick={() => openModalBatmanHandler()}>
         <img src="https://i.ibb.co/M18p91c/batman.webp" alt="batman" />
       </div>
     </div>
