4ffe4b36dcb86f6683b4f1e978e585ea1c67b416
Christian <c.espinoza.code@gmail.com>
2023-07-01 23:53:46 -0500

    images added to each radio

--- src/components/Modals/Spiderman/index.jsx
+++ src/components/Modals/Spiderman/index.jsx
@@ -26,14 +26,20 @@ function ModalSpiderman() {
           <div className="cards">
             <label htmlFor="radio-1" id="card-1" className="card">
               <img
+                src="https://static.platzi.com/media/public/uploads/spider-man_db8af43d-b692-4986-a385-e645d286e142.png"
+                alt="Spiderman"
               />
             </label>
             <label htmlFor="radio-2" id="card-2" className="card">
               <img
+                src="https://static.platzi.com/media/public/uploads/spider-man-2_b24c96b3-6d59-496f-815d-32b25eba0141.png"
+                alt="Spiderman"
               />
             </label>
             <label htmlFor="radio-3" id="card-3" className="card">
               <img
+                src="https://static.platzi.com/media/public/uploads/spider-man-3_19f5c510-8059-4477-abf8-42249a5445b7.png"
+                alt="Spiderman"
               />
             </label>
           </div>
