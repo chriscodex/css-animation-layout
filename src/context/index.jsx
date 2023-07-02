e3f0e0453036822948df910816ae32bd19f78878
Christian <c.espinoza.code@gmail.com>
2023-07-02 00:05:52 -0500

    state for robin & batman modal added

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,9 +4,20 @@ const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [openModalSpiderman, setOpenModalSpiderman] = useState(false);
+  const [openModalRobin, setOpenModalRobin] = useState(false);
+  const [openModalBatman, setOpenModalBatman] = useState(false);
 
   return (
-    <AppContext.Provider value={{ openModalSpiderman, setOpenModalSpiderman }}>
+    <AppContext.Provider
+      value={{
+        openModalSpiderman,
+        setOpenModalSpiderman,
+        openModalRobin,
+        setOpenModalRobin,
+        openModalBatman,
+        setOpenModalBatman,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
