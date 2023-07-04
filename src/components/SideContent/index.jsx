0135d9e0a87a115844ed82398ec104136bbd45bb
Christian <c.espinoza.code@gmail.com>
2023-07-03 21:46:29 -0500

    shadow added to side background & footer

--- src/components/SideContent/index.jsx
+++ src/components/SideContent/index.jsx
@@ -3,7 +3,7 @@ import './styles.css';
 function SideContent() {
   return (
     <div className="side-content">
-      <div></div>
+      <div className='side-content__font'></div>
       <p>Superman</p>
       <img src="https://i.ibb.co/x16pz1x/super-man.png" alt="superman" />
     </div>
